7fc2152c8459aabb8051cd9585ce689e46932bfe
ricardoreisdata <ricardoreis.data@gmail.com>
2023-10-09 23:42:24 -0300

    Tarefa 2023-10-06

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -4,7 +4,26 @@ from cadastro import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('teste', views.teste, name='teste'),
-    path('listar_cursos', views.listar_cursos),
-    path('listar_professores', views.listar_professores),
-    path('listar_alunos', views.listar_alunos)
+    
+    path('listar_cursos', views.listar_cursos, name='listar_cursos'),
+    path('incluir_curso', views.incluir_curso, name='incluir_curso'),
+    path('editar_curso/<int:id>', views.editar_curso, name='editar_curso'),
+    path('excluir_curso/<int:id>', views.excluir_curso,name='excluir_curso'),
+    
+
+    path('listar_professores', views.listar_professores, name='listar_professores'),
+    path('incluir_professor', views.incluir_professor, name='incluir_professor'),
+    path('editar_professor/<int:id>', views.editar_professor, name='editar_professor'),
+    path('excluir_professor/<int:id>', views.excluir_professor,name='excluir_professor'),
+
+    path('listar_alunos', views.listar_alunos, name='listar_alunos'),
+    path('incluir_aluno', views.incluir_aluno, name='incluir_aluno'),
+    path('editar_aluno/<int:id>', views.editar_aluno, name='editar_aluno'),
+    path('excluir_aluno/<int:id>', views.excluir_aluno,name='excluir_aluno'),
+    
+    
+    
+    
+    
+    
 ]
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from cadastro.models import Curso, Professor, Aluno
+from cadastro.forms import CursoForm, AlunoForm, ProfessorForm
 
 def index(request):
     return HttpResponse('Olá Mundo!!! Agora estou na web')
@@ -16,9 +17,117 @@ def listar_cursos(request):
     return render(request, template_name='listar_cursos.html', context={'lista':cursos})
 
 def listar_professores(request):
-    professores = Professor.objetcs.all().order_by('nome')
+    professores = Professor.objects.all().order_by('nome')
     return render(request, template_name='listar_professores.html', context={'lista':professores})
 
 def listar_alunos(request):
     alunos = Aluno.objects.all().order_by('nome')
     return render(request, template_name='listar_alunos.html', context={'lista':alunos})
+
+def incluir_curso(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                model = form.instance
+                return redirect('listar_cursos')
+            except:
+                pass
+    else:
+        form = CursoForm()
+    return render(request, template_name='incluir_curso.html', context={'form':form})
+
+def editar_curso(request, id):
+    curso = Curso.objects.get(id_curso = id)
+    form = CursoForm(instance=curso)
+
+    if request.method == "POST":
+        form = CursoForm(request.POST, instance=curso)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('listar_cursos')
+            except:
+                pass
+    return render(request, template_name='incluir_curso.html', context={'form':form})
+
+def excluir_curso(request, id):
+    curso = Curso.objects.get(id_curso = id)
+    try:
+        curso.delete()
+    except:
+        pass
+    return redirect('listar_cursos')
+
+def incluir_aluno(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                model = form.instance
+                return redirect('listar_alunos')
+            except:
+                pass
+    else:
+        form = AlunoForm()
+    return render(request, template_name='incluir_aluno.html', context={'form':form})
+
+def incluir_professor(request):
+    if request.method == 'POST':
+        form = ProfessorForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                model = form.instance
+                return redirect('listar_professores')
+            except:
+                pass
+    else:
+        form = ProfessorForm()
+    return render(request, template_name='incluir_professor.html', context={'form':form})
+
+def editar_aluno(request, id):
+    aluno = Aluno.objects.get(id_aluno = id)
+    form = AlunoForm(instance=aluno)
+
+    if request.method == "POST":
+        form = AlunoForm(request.POST, instance=aluno)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('listar_alunos')
+            except:
+                pass
+    return render(request, template_name='incluir_aluno.html', context={'form':form})
+
+def editar_professor(request, id):
+    professor = Professor.objects.get(id_professor = id)
+    form = ProfessorForm(instance=professor)
+
+    if request.method == "POST":
+        form = ProfessorForm(request.POST, instance=professor)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('listar_professores')
+            except:
+                pass
+    return render(request, template_name='incluir_professor.html', context={'form':form})
+
+def excluir_aluno(request, id):
+    aluno = Aluno.objects.get(id_aluno = id)
+    try:
+        Aluno.delete()
+    except:
+        pass
+    return redirect('listar_alunos')
+
+def excluir_professor(request, id):
+    professor = Professor.objects.get(id_professor = id)
+    try:
+        professor.delete()
+    except:
+        pass
+    return redirect('listar_professores')
