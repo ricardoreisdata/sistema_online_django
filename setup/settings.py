c0952461ef86c366a5351c5ddc25dd2d7803d96d
ricardoreisdata <ricardoreis.data@gmail.com>
2023-10-13 16:34:48 -0300

    instalacao django-bootstrap-v5 e unistrall old

--- setup/settings.py
+++ setup/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cadastro'
+    'cadastro',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
